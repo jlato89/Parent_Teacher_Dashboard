0fe32c0d9fc2655004d0aa66c76bcbcddc2bb7cf
Josh Latour <jlatour88@gmail.com>
2019-10-14 14:40:06 -0500

    placeholder for menu

---
+++ client/src/components/Menu/Menu.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Menu() {
+   return (
+      <div className='menu'>
+         {/* Placeholder Ideas for Menu */}
+         unreadMail | Logout
+         profileImg
+         editProfile
+      </div>
+   )
+}
+
+export default Menu
