811725a5b9bd565073a78cf04a2143dba4bd84c5
Josh Latour <jlatour88@gmail.com>
2019-10-15 11:36:04 -0500

    combined parent and teacher tables into one.

---
+++ models/User.js
@@ -0,0 +1,58 @@
+module.exports = function(sequelize, Sequelize) {
+   const User = sequelize.define('user', {
+      id: {
+         type: Sequelize.UUID,
+         primaryKey: true,
+         defaultValue: Sequelize.UUIDV4
+      },
+      fullName: {
+         type: Sequelize.STRING,
+         allowNull: false
+      },
+      userName: {
+         type: Sequelize.STRING,
+         allowNull: false
+      },
+      password: {
+         type: Sequelize.TEXT,
+         allowNull: false
+      },
+      isTeacher: {
+         type: Sequelize.BOOLEAN,
+         defaultValue: 0
+      },
+      isDirector: {
+         type: Sequelize.BOOLEAN,
+         defaultValue: 0
+      },
+      fullName2: {
+         type: Sequelize.STRING
+      },
+      email: {
+         type: Sequelize.STRING
+      },
+      profileImage: {
+         type: Sequelize.TEXT
+      },
+      phone: {
+         type: Sequelize.INTEGER
+      },
+      address: {
+         type: Sequelize.STRING
+      },
+      emergencyName: {
+         type: Sequelize.STRING
+      },
+      emergencyPhone: {
+         type: Sequelize.INTEGER
+      },
+      emergencyRelation: {
+         type: Sequelize.STRING
+      },
+      approvedNames: {
+         type: Sequelize.STRING
+      }
+   });
+
+   return User;
+}
