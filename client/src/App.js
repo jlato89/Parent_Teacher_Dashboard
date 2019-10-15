d87739bb5ea2f7010ea5de903491fdf7fce6a3b6
Josh Latour <jlatour88@gmail.com>
2019-10-15 14:04:25 -0500

    removed unneeded state

--- client/src/App.js
+++ client/src/App.js
@@ -5,13 +5,6 @@ import Dashboard from './pages/Dashboard';
 import NoMatch from './pages/NoMatch';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      authType: ''
-    };
-  }
-
   render() {
     return (
       <>
