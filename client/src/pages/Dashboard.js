1a388de20c7e19ed4b8a12f5f09190157ad54a44
Josh Latour <jlatour88@gmail.com>
2019-10-15 00:21:53 -0500

    added new conditional placeholder

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -12,16 +12,22 @@ class Dashboard extends Component {
   }
 
   render() {
+    let buttons = null; //? Set null initially
+    if (this.state.showBtns) {
+      //? If showBtns is true, update buttons with content
+      buttons = (
+        <div>
+          <div className='addReport'>Add Report</div>
+          <div className='addEvent'>Add Event</div>
+          <div className='viewKids'>View Kids</div>
+        </div>
+      )
+    }
+
     return (
       <div className='dashboardPage'>
         <Banner userType={this.state.userType} />
-        {this.state.showBtns ? (
-          <div>
-            <div className='addReport'>Add Report</div>
-            <div className='addEvent'>Add Event</div>
-            <div className='viewKids'>View Kids</div>
-          </div>
-        ) : null}
+        {buttons}
       </div>
     );
   }
