d2f1cfacc3c1dc0747ab3001a64c27b3e43464ed
Josh Latour <jlatour88@gmail.com>
2019-10-15 13:33:38 -0500

    removed parent and teacher dash

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
-import TeacherDash from './pages/TeacherDash';
-import ParentDash from './pages/ParentDash';
-import NoMatch from './pages/NoMatch';
 import Dashboard from './pages/Dashboard';
+import NoMatch from './pages/NoMatch';
 
 class App extends Component {
   constructor() {
@@ -21,8 +19,6 @@ class App extends Component {
           <Switch>
             <Route exact path='/' component={Login} />
             <Route exact path='/dashboard' component={Dashboard} />
-            <Route exact path='/teacherdash' component={TeacherDash} />
-            <Route exact path='/parentdash' component={ParentDash} />
             <Route component={NoMatch} />
           </Switch>
         </Router>
