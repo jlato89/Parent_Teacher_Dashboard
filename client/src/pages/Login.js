d5ad114d557d8243378eb088f277342c59e07093
Josh Latour <jlatour88@gmail.com>
2019-10-16 20:42:07 -0500

    Tweaked login design, removed extra designs for something more basic

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,38 +1,12 @@
 import React, { Component } from 'react';
 
 const styles = {
-  appHeader: {
-    background:
-      'linear-gradient(to right, rgba(146, 254, 157,0.4) 0%, rgba(0, 201, 255, 0.4) 100%)',
-    height: 300,
-    fontWeight: 300,
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center'
-  },
   header: {
-    fontSize: '2.5rem',
+    fontSize: '2rem',
     textAlign: 'center',
     textTransform: 'uppercase',
     letterSpacing: 1.1,
     fontWeight: '300'
-  },
-  subHeader: {
-    fontSize: '1.5rem'
-  },
-  cardWrapper: {
-    position: 'absolute',
-    left: '50%',
-    marginLeft: '-175px',
-    top: '35%',
-    marginTop: '-100px',
-    borderRadius: 10,
-    backgroundColor: '#edf2f7',
-    width: 375,
-    padding: 50,
-    maxWidth: '90%',
-    boxShadow: '0 12px 15px rgba(0,0,0,0.1), 0 17px 50px rgba(0,0,0,0.1)'
-    // transform: 'translateY(-250px)'
   }
 };
 
@@ -67,53 +41,59 @@ class Login extends Component {
     const { errors } = this.state;
 
     return (
-      <div style={{ position: 'relative' }}>
-        <div style={styles.appHeader}></div>
+      <div>
+        <h1 style={styles.header}>
+          PT Dashboard <br />
+        </h1>
 
-        <div style={styles.cardWrapper}>
-          <h1 style={styles.header}>
-            The Dashboard <br />
-            <span style={styles.subHeader}>Login</span>
-          </h1>
+        <img
+          src={require('../assets/images/happy-children-and-daycare.png')}
+          alt='paper plane logo'
+          style={{ height: 200, margin: '30px auto', display: 'block' }}
+        />
 
-          <img
-            src={require('../assets/images/happy-children-and-daycare.png')}
-            alt='paper plane logo'
-            style={{ height: 200, margin: '30px auto', display: 'block' }}
+        <form
+          style={{ margin: '0 auto', width: 'fit-content' }}
+          onSubmit={this.onSubmit}
+          className='ui form'
+        >
+          <div className={`field ${errors.userName ? 'error' : ''}`}>
+            <label>
+              {errors.userName ? <p>{errors.userName}</p> : 'Username'}
+            </label>
+            <br />
+            <input
+              autoComplete='username'
+              name='Username'
+              type='Username'
+              value={this.state.userName}
+              onChange={this.onChange}
+            />
+          </div>
+
+          <div
+            style={{ margin: '6px 0' }}
+            className={`field ${errors.password ? 'error' : ''}`}
+          >
+            <label>
+              {errors.password ? <p>{errors.password}</p> : 'Password'}
+            </label>
+            <br />
+            <input
+              autoComplete='password'
+              name='password'
+              type='password'
+              value={this.state.password}
+              onChange={this.onChange}
+            />
+          </div>
+
+          <input
+            style={{ float: 'right' }}
+            type='submit'
+            className='ui button grey fluid'
           />
-
-          <form onSubmit={this.onSubmit} className='ui form'>
-            <div className={`field ${errors.email ? 'error' : ''}`}>
-              <label>
-                {errors.email ? <p>{errors.email}</p> : 'Email Address'}
-              </label>
-              <input
-                placeholder='Email Address'
-                name='email'
-                type='email'
-                value={this.state.email}
-                onChange={this.onChange}
-              />
-            </div>
-
-            <div className={`field ${errors.password ? 'error' : ''}`}>
-              <label>
-                {errors.password ? <p>{errors.password}</p> : 'Password'}
-              </label>
-              <input
-                autoComplete='password'
-                placeholder='Password'
-                name='password'
-                type='password'
-                value={this.state.password}
-                onChange={this.onChange}
-              />
-            </div>
-            
-            <input type='submit' className='ui button grey fluid' />
-          </form>
-          <div style={{ marginTop: 20, textAlign: 'center' }}></div>
-        </div>
+        </form>
       </div>
     );
   }
