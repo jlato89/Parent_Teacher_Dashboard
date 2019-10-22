8979367828ab35c02bc47467b293ea6d7f98dff2
Josh Latour <jlatour88@gmail.com>
2019-10-22 17:14:32 -0500

    added find by id route

--- routes/studentRoutes/findStudents.js
+++ routes/studentRoutes/findStudents.js
@@ -13,4 +13,23 @@ module.exports = app => {
         res.status(500).send(err.response);
       });
   });
+
+  app.get('/findStudent/:parentId', (req, res) => {
+    const parentId = req.params.parentId;
+
+    db.student
+      .findOne({
+        where: {
+          parentId: parentId
+        }
+      })
+      .then(student => {
+        // console.log(student);
+        res.status(200).send(student);
+      })
+      .catch(err => {
+        console.log(err.response);
+        res.status(500).send(err.response);
+      });
+  });
 };
