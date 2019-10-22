cd58f290ded50b52768c1d26b8f6b0618533f629
Josh Latour <jlatour88@gmail.com>
2019-10-22 17:13:47 -0500

    removed static input and just passed data without the psw to DB

--- routes/userRoutes/registerUser.js
+++ routes/userRoutes/registerUser.js
@@ -12,36 +12,21 @@ module.exports = app => {
         res.send(info.message);
       } else {
         req.logIn(user, err => {
-          const data = {
-            fullName: req.body.fullName,
-            userName: req.body.userName,
-            email: req.body.email,
-            isTeacher: req.body.isTeacher,
-            isDirector: req.body.isDirector,
-            fullName2: req.body.fullName2,
-            profileImage: req.body.profileImage,
-            phone: req.body.phone,
-            address: req.body.address,
-            emergencyName: req.body.emergencyName,
-            emergencyPhone: req.body.emergencyPhone,
-            emergencyRelation: req.body.emergencyRelation,
-            approvedNames: req.body.approvedNames
-          };
-          db.user.findOne({
-            where: {
-              userName: data.userName
-            }
-          }).then(user => {
-            user
-              .update({
-                fullName: data.fullName
-                //! add more fields
-              })
-              .then(() => {
+          const data = req.body;
+          delete data.password;
+
+          db.user
+            .findOne({
+              where: {
+                userName: data.userName
+              }
+            })
+            .then(user => {
+              user.update(data).then(() => {
                 console.log('user created in db');
                 res.status(200).send({ message: 'user created' });
               });
-          });
+            });
         });
       }
     })(req, res, next);
