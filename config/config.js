911c72e7af4ce22ea1c4fb625c84043e8992df4c
Josh Latour <jlatour88@gmail.com>
2019-10-22 01:01:36 -0500

    added local config for offline use

--- config/config.js
+++ config/config.js
@@ -1,26 +1,26 @@
 module.exports = {
-   development: {
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      host: process.env.DB_HOST,
-      dialect: 'mysql',
-      logging: false,
-   },
-   test: {
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      host: process.env.DB_HOST,
-      dialect: 'mysql',
-      logging: false,
-   },
-   production: {
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      host: process.env.DB_HOST,
-      dialect: 'mysql',
-      logging: false,
-   },
-}
+  local: {
+    username: 'root',
+    password: 'password',
+    database: 'ptdash',
+    host: 'localhost',
+    dialect: 'mysql',
+    logging: false
+  },
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    logging: false
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    logging: false
+  }
+};
