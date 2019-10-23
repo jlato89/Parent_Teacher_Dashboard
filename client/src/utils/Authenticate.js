a1298fd6019a64d1dbfd20db48a17e1a136468da
Josh Latour <jlatour88@gmail.com>
2019-10-22 21:21:15 -0500

    idea to pass usertype from token to dashboard to set in state instead of doing axios call

--- client/src/utils/Authenticate.js
+++ client/src/utils/Authenticate.js
@@ -2,11 +2,16 @@ import jwt_decode from 'jwt-decode';
 
 const Authenticate = () => {
   let tokenValid = true;
+  // let userType;
   let localToken = localStorage.getItem('ptDash');
 
   if (localToken) {
     const decode = jwt_decode(localToken);
     const currentTime = new Date().getTime() / 1000;
+    //! Idea to add userType to token and pass to dashboard
+    // userType = decode.userType;
+
+    // console.log(decode);
 
     if (currentTime > decode.exp) {
       tokenValid= false;
