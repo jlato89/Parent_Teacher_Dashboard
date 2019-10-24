6fd475941454fc7b465634a2b0cd5bd8859b48af
Josh Latour <jlatour88@gmail.com>
2019-10-24 12:36:46 -0500

    Moved styling to css file, added classnames for future use, removed anchor tags

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,29 +1,14 @@
 import React from 'react';
 import MailIcon from '../../assets/images/mail-24px.svg';
 import MenuIcon from '../../assets/images/menu-24px.svg';
+import './Header-module.css';
 
-const styles = {
-  header: {
-    backgroundColor: '#98CEFF',
-    display: 'flex',
-    justifyContent: 'space-between',
-    padding: '5px',
-    fontSize: '1.4rem',
-    fontWeight: 'bold'
-  },
-  menuBtn: {
-    height: '30px'
-  }
-};
-
-function Header(props) {
+function Header() {
   return (
-    <div style={styles.header}>
-      <a href='/mail'>
-        <img src={MailIcon} alt='Mail Icon' height='30px' />
-      </a>
-      <span>PT Dashboard</span>
-      <img src={MenuIcon} alt='Menu Icon' height='30px' />
+    <div className='header'>
+      <img className='mailBtn' src={MailIcon} alt='Mail Icon' height='30px' />
+      <span className='headerText'>PT Dashboard</span>
+      <img className='menuBtn' src={MenuIcon} alt='Menu Icon' height='30px' />
     </div>
   );
 }
