18908da14d24c3daf9bf8358e4d4f54895b88b29
Josh Latour <jlatour88@gmail.com>
2019-10-25 18:59:28 -0500

    added greeting msg conditional determined by time

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -4,6 +4,18 @@ import SearchIcon from '../../assets/images/outline_search_white_24.png';
 import styles from './Header.module.css';
 import ProfileImgDefault from '../../assets/images/outline_account_circle_white_48.png';
 
+const today = new Date();
+const curHr = today.getHours()
+let greetingMsg;
+
+if (curHr < 12) {
+  greetingMsg = 'Good Morning';
+} else if (curHr < 18) {
+  greetingMsg = 'Good Afternoon';
+} else {
+  greetingMsg = 'Good Evening';
+}
+
 function Header(props) {
   let profileImg = props.profileImg
   if (!props.profileImg) {
@@ -36,7 +48,7 @@ function Header(props) {
       </div>
       <div className={styles.bottomHeaderText}>
         <p>
-          <span className={styles.bold}>Hi {props.name},</span> Good MORNING
+          <span className={styles.bold}>Hi {props.name},</span> {greetingMsg}
         </p>
         <p>Today, 14 Nov 2019</p>
       </div>
