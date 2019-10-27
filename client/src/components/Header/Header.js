8a571f38b4d9672a89e1dc4ae874097ce236e4b0
Josh Latour <jlatour88@gmail.com>
2019-10-27 14:35:00 -0500

    set more dynamic header(full and miniHeader)

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import MailIcon from '../../assets/images/outline_mail_white_24.png';
 import SearchIcon from '../../assets/images/outline_search_white_24.png';
-import styles from './Header.module.css';
 import ProfileImgDefault from '../../assets/images/outline_account_circle_white_48.png';
+import styles from './Header.module.css';
 
-const today = new Date();
-const curHr = today.getHours()
+const curHr = new Date().getHours();
 let greetingMsg;
-
 if (curHr < 12) {
   greetingMsg = 'Good Morning';
 } else if (curHr < 18) {
@@ -17,41 +15,41 @@ if (curHr < 12) {
 }
 
 function Header(props) {
-  let profileImg = props.profileImg
+  let miniHeader = false;
+  let profileImg = props.profileImg;
   if (!props.profileImg) {
     profileImg = ProfileImgDefault;
+  }
+  if (props.miniHeader) {
+    miniHeader = true;
   }
 
   return (
     <div className={styles.container}>
       <div className={styles.topHeader}>
-        <img
-          src={MailIcon}
-          alt='Mail Icon'
-          height='30px'
-        />
-        {/* <span className={styles.topHeaderText}>
-          <a href='/dashboard'>
-            PT Dashboard
-          </a>
-        </span> */}
-        <img
-          className={styles.profileImg}
-          src={profileImg}
-          alt='Profile Img'
-        />
-        <img
-          src={SearchIcon}
-          alt='Menu Icon'
-          height='30px'
-        />
+        <img src={MailIcon} alt='Mail Icon' height='30px' />
+        {miniHeader && (
+        <span className={styles.topHeaderText}>
+          {props.title}
+        </span>
+        )}
+        {!miniHeader && (
+          <img
+            className={styles.profileImg}
+            src={profileImg}
+            alt='Profile Img'
+          />
+        )}
+        <img src={SearchIcon} alt='Menu Icon' height='30px' />
       </div>
-      <div className={styles.bottomHeaderText}>
-        <p>
-          <span className={styles.bold}>Hi {props.name},</span> {greetingMsg}
-        </p>
-        <p>Today, 14 Nov 2019</p>
-      </div>
+      {!miniHeader && (
+        <div className={styles.bottomHeaderText}>
+          <p>
+            <span className={styles.bold}>Hi {props.name},</span> {greetingMsg}
+          </p>
+          <p>Today, 14 Nov 2019</p>
+        </div>
+      )}
     </div>
   );
 }
