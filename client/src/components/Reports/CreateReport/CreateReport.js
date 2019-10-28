631ee0e9b76d4836db07b4e78f1b231fafbc6898
Josh Latour <jlatour88@gmail.com>
2019-10-28 13:04:20 -0500

    added radioBtns, setup axios post call, added validation check for empty

--- client/src/components/Reports/CreateReport/CreateReport.js
+++ client/src/components/Reports/CreateReport/CreateReport.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import isEmpty from '../../../validation/is-empty';
 import Header from '../../Header/Header';
 import styles from './CreateReport.module.css';
 
@@ -8,6 +9,7 @@ class CreateReport extends Component {
     super();
     this.state = {
       redirect: false,
+      errors: '',
       name: '',
       date: '',
       attitude: '',
@@ -29,103 +31,151 @@ class CreateReport extends Component {
   onSubmit = e => {
     e.preventDefault();
 
-    const reportData = {
-      name: this.state.name,
-      date: this.state.date,
-      attitude: this.state.attitude,
-      enjoyed: this.state.enjoyed,
-      brBreaks: this.state.brBreaks,
-      napTime: this.state.napTime,
-      ate: this.state.ate,
-      suppliesNeeded: this.state.suppliesNeeded,
-      comments: this.state.comments,
-    };
-
-    axios
-      .post('/createReport', reportData)
-      .then(response => {
-        if (response.data.token) {
-          const { token } = response.data;
-
-          localStorage.setItem('ptDash', token);
-
-          this.setState({
-            errors: '',
-            redirect: true
-          });
-        }
-        console.log(response.data);
-      })
-      .catch(err => {
-        console.log(err.response.data);
-        this.setState({
-          errors: err.response.data
-        });
+    if (
+      isEmpty(this.state.name) ||
+      isEmpty(this.state.date)
+    ) {
+      this.setState({
+        errors: 'Please fill out missing fields!'
       });
+    } else {
+      this.setState({ errors: '' });
+      const reportObj = {
+        studentName: this.state.name,
+        date: this.state.date,
+        attitude: this.state.attitude,
+        enjoyed: this.state.enjoyed,
+        brBreaks: this.state.brBreaks,
+        napTime: this.state.napTime,
+        ate: this.state.ate,
+        suppliesNeeded: this.state.suppliesNeeded,
+        comments: this.state.comments
+      };
+
+      axios
+        .post('/reports/createReport', reportObj)
+        .then(response => {
+          console.log(response.data);
+        })
+        .catch(err => {
+          console.log(err.response.data);
+          this.setState({
+            errors: err.response.data
+          });
+        });
+    }
   };
 
   render() {
+    const { errors } = this.state;
     return (
       <div>
-        <Header miniHeader={true} title='Create Report'/>
+        <Header miniHeader={true} title='Create Report' />
+        {/* Show error message if error state is true */}
+        {errors && <div className={styles.errors}>{errors}</div>}
+
         <form onSubmit={this.onSubmit}>
-          <label className={styles.formLabel}>Name</label>
+          <label className={styles.formInputTitle}>Name*</label>
           <input
             name='name'
             type='text'
             value={this.state.name}
             onChange={this.onChange}
           />
-          <label className={styles.formLabel}>Date</label>
+          <label className={styles.formInputTitle}>Date*</label>
           <input
             name='date'
             type='date'
             value={this.state.date}
             onChange={this.onChange}
           />
-          <label className={styles.formLabel}>Today I was?</label>
-          <input
-            name='attitude'
-            type='text'
-            value={this.state.attitude}
-            onChange={this.onChange}
-          />
-          <label className={styles.formLabel}>Today I enjoyed?</label>
+          <label className={styles.formInputTitle}>Today I was?*</label>
+          <label className={styles.radioBtn}>
+            <input
+              className={styles.hidden}
+              type='radio'
+              name='attitude'
+              value='option1'
+              onChange={this.onChange}
+            />
+            Angry
+          </label>
+          <label className={styles.radioBtn}>
+            <input
+              className={styles.hidden}
+              type='radio'
+              name='attitude'
+              value='option2'
+              onChange={this.onChange}
+            />
+            Sad
+          </label>
+          <label className={styles.radioBtn}>
+            <input
+              className={styles.hidden}
+              type='radio'
+              name='attitude'
+              value='option3'
+              onChange={this.onChange}
+            />
+            Neutral
+          </label>
+          <label className={styles.radioBtn}>
+            <input
+              className={styles.hidden}
+              type='radio'
+              name='attitude'
+              value='option4'
+              onChange={this.onChange}
+            />
+            Happy
+          </label>
+          <label className={styles.radioBtn}>
+            <input
+              className={styles.hidden}
+              type='radio'
+              name='attitude'
+              value='option5'
+              onChange={this.onChange}
+            />
+            Joyful
+          </label>
+          <label className={styles.formInputTitle}>Today I enjoyed?*</label>
           <input
             name='enjoyed'
             type='text'
             value={this.state.enjoyed}
             onChange={this.onChange}
           />
-          <label className={styles.formLabel}>Bathroom Breaks</label>
+          <label className={styles.formInputTitle}>Bathroom Breaks*</label>
           <input
             name='brBreaks'
             type='text'
             value={this.state.brBreaks}
             onChange={this.onChange}
           />
-          <label className={styles.formLabel}>Nap Time</label>
+          <label className={styles.formInputTitle}>Nap Time*</label>
           <input
             name='napTime'
             type='time'
             value={this.state.napTime}
             onChange={this.onChange}
           />
-          <label className={styles.formLabel}>Today I Ate</label>
+          <label className={styles.formInputTitle}>Today I Ate*</label>
           <input
             name='ate'
             type='text'
             value={this.state.ate}
             onChange={this.onChange}
           />
-          <label className={styles.formLabel}>Supplies Needed</label>
+          <label className={styles.formInputTitle}>Supplies Needed</label>
           <input
             name='suppliesNeeded'
             type='text'
             value={this.state.suppliesNeeded}
             onChange={this.onChange}
           />
-          <label className={styles.formLabel}>Comments</label>
+          <label className={styles.formInputTitle}>Comments</label>
           <input
             name='comments'
             type='textarea'
