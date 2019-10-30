ee2af14ab7b94023db835eae99bef4b6c500b791
Josh Latour <jlatour88@gmail.com>
2019-10-30 00:11:48 -0500

    retrieve dashLinks as array then map and output them.

--- client/src/components/MenuBtn/MenuBtn.js
+++ client/src/components/MenuBtn/MenuBtn.js
@@ -2,17 +2,16 @@ import React from 'react';
 import styles from './MenuBtn.module.css';
 
 const MenuBtn = props => {
-  let menuBtns = (
-    <>
-      <a href={props.link} className={styles.menuBtn}>
-        <span className={styles.menuBtnTitle}>{props.name}</span>
-      </a>
-      <a href={props.link} className={styles.menuBtn}>
-        <span className={styles.menuBtnTitle}>{props.name}</span>
-      </a>
-    </>
+  return (
+    <div className={styles.container}>
+      {props.dashLinks.map(link => (
+        <a className={styles.menuBtn} key={link.name} href={link.url}>
+          <span className={styles.menuBtnTitle}>{link.name}</span>
+        </a>
+      ))}
+      ;
+    </div>
   );
-  return <div className={styles.container}>{menuBtns}</div>;
 };
 
 export default MenuBtn;
