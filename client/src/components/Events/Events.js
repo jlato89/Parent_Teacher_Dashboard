387b7b98579a2732dbcbf3d5938f99e062c797b8
Josh Latour <jlatour88@gmail.com>
2019-10-30 14:02:58 -0500

    extracted non announcements from events, conditional to show or hide events

--- client/src/components/Events/Events.js
+++ client/src/components/Events/Events.js
@@ -3,10 +3,17 @@ import Event from './Event/Event';
 import styles from './Events.module.css';
 
 const Events = props => {
+  //? Grab only events and leave announcements out
+  const events = props.events.filter(event => {
+    return event.isAnnouncement === false;
+  });
+
   return (
+    events ?
     <div className={styles.container}>
-      <Event events={props.events} />
+      <Event events={events} />
     </div>
+    : null
   )
 }
 
