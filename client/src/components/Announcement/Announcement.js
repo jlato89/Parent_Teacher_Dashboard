fcaddedec93cc007c508973d2a382ec898324e1c
Josh Latour <jlatour88@gmail.com>
2019-10-30 14:22:30 -0500

    added conditional and moved filter to this file instead of dashboard

--- client/src/components/Announcement/Announcement.js
+++ client/src/components/Announcement/Announcement.js
@@ -3,27 +3,26 @@ import Moment from 'react-moment';
 import styles from './Announcement.module.css';
 
 const Announcement = props => {
-  let announcements;
-  if (props.announcements.length > 0) {
-    announcements = (
+  const announcements = props.events.filter(event => {
+    return event.isAnnouncement === true;
+  });
+
+  return (
+    announcements ?
       <div className={styles.container}>
-        {props.announcements.map(announcement => {
+        {announcements.map(announcement => {
           return (
             <div key={announcement.id}>
               <span className={styles.date}>
                 (<Moment date={announcement.eventDate} format='MM/DD' />)
               </span>
-              <span className={styles.desc}>
-                {announcement.description}
-              </span>
+              <span className={styles.desc}>{announcement.description}</span>
             </div>
           );
         })}
       </div>
-    );
-  }
-
-  return <>{announcements}</>;
+    : null
+  );
 };
 
 export default Announcement;
