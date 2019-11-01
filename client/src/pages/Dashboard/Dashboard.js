193d734352b39e3e26234bf843e23a82b6aeba3f
Josh Latour <jlatour88@gmail.com>
2019-11-01 13:16:13 -0500

    removed student axios call, commented out token user initial state set, passed full eventArr to needed components

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 import axios from 'axios';
 import setAuthToken from '../../utils/setAuthToken';
-import jwt_decode from 'jwt-decode';
+// import jwt_decode from 'jwt-decode';
 import Layout from '../../components/Layout/Layout';
 import Announcements from '../../components/Announcement/Announcement';
 import DashBtns from '../../components/DashBtns/DashBtns';
@@ -55,38 +55,28 @@ class Dashboard extends Component {
     const token = localStorage.getItem('ptDash');
     if (token) {
       setAuthToken(token);
-      const decode = jwt_decode(token);
-      await this.setState({
-        user: {
-          id: decode.id,
-          userType: decode.userType
-        }
-      });
-    }
-
-    //? Check if parent, if true grab students associated with the user ID
-    let findStudent = '/api/findStudent';
-    if (this.state.user.userType === 'parent') {
-      console.log('User is parent');
-      findStudent = `/api/findStudent/${this.state.user.id}`;
+      // const decode = jwt_decode(token);
+      // await this.setState({
+      //   user: {
+      //     id: decode.id,
+      //     userType: decode.userType
+      //   }
+      // });
     }
 
     //? Run all calls for info that needs to be retrieved.
     axios
       .all([
         axios.get('/api/findUser'),
-        axios.get('/api/findEvent'),
-        axios.get(findStudent)
+        axios.get('/api/findEvent')
       ])
       .then(
-        axios.spread((userData, eventData, studentData) => {
+        axios.spread((userData, eventData) => {
           console.log('User:', userData.data.user);
           console.log('Events:', eventData.data);
-          console.log('Students:', studentData.data);
           this.setState({
             user: userData.data.user,
             eventArr: eventData.data,
-            studentArr: studentData.data,
             loading: false
           });
         })
@@ -101,12 +91,12 @@ class Dashboard extends Component {
 
   render() {
     const { redirect, user } = this.state;
-        
+
     //? Redirect to homepage if logged-out
     if (redirect) {
       return <Redirect to='/' />;
     }
-    
+
     //? --------------------------------------------------------
     //? Content to render when app is loading
     let dashboardContent;
@@ -125,7 +115,7 @@ class Dashboard extends Component {
           <Layout profileImg={user.profileImage} name={user.firstName}>
             <Announcements events={this.state.eventArr} />
             <DashBtns dashBtns={this.state.dashBtns} />
-            <Events events={this.state.eventArr}/>
+            <Events events={this.state.eventArr} />
           </Layout>
           <button onClick={this.handleLogout}>Logout</button>
         </>
