233bdff8fd97f1e8ce3d91cd37751a5ef9d99600
Josh Latour <jlatour88@gmail.com>
2019-11-06 10:18:51 -0600

    added basic delete route method

--- routes/userRoutes/deleteUser.js
+++ routes/userRoutes/deleteUser.js
@@ -1,3 +1,24 @@
+const db = require('../../models');
+
 module.exports = app => {
-  
-}
+  app.delete('/api/deleteUser', (req, res) => {
+    db.user
+      .destroy({
+        where: {
+          id: req.body.userId 
+          //! maybe add a validation check to be sure request
+          //! came from valid server.
+        }
+      })
+      .then(rowDeleted => {
+        if (rowDeleted === 1) {
+          console.log('Deleted successfully');
+          res.status(200).json({ msg: 'Deleted successfully'});
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({ msg: 'There was an error' });
+      });
+  });
+};
