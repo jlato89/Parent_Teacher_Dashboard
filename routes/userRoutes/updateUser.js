c6c0eb37640459f561e7365599120293974a3d7d
Josh Latour <jlatour88@gmail.com>
2019-11-05 20:57:25 -0600

    added basic update model route

--- routes/userRoutes/updateUser.js
+++ routes/userRoutes/updateUser.js
@@ -1,3 +1,17 @@
+const db = require('../../models');
+
 module.exports = app => {
-  
-}
+  app.put('/api/updateUser', (req, res) => {
+    db.user.update(req.body, {
+      where: { id: req.body.id }
+    })
+    .then(result => {
+      console.log('Records Updated: ', result);
+      res.status(200).json(result)
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(result);
+    });
+  });
+};
