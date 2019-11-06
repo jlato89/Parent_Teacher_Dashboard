e63af228dce5ba55f339804da203cd0a637137f1
Josh Latour <jlatour88@gmail.com>
2019-11-06 10:21:10 -0600

    added better json msg

--- routes/userRoutes/updateUser.js
+++ routes/userRoutes/updateUser.js
@@ -2,16 +2,19 @@ const db = require('../../models');
 
 module.exports = app => {
   app.put('/api/updateUser', (req, res) => {
-    db.user.update(req.body, {
-      where: { id: req.body.id }
-    })
-    .then(result => {
-      console.log('Records Updated: ', result);
-      res.status(200).json(result)
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(result);
-    });
+    db.user
+      .update(req.body, {
+        where: { id: req.body.id }
+      })
+      .then(rowUpdated => {
+        if (rowUpdated === 1) {
+          console.log('Updated successfully');
+          res.status(200).json({ msg: 'Updated successfully' });
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({ msg: 'There was an error' });
+      });
   });
 };
