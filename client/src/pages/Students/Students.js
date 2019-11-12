ca463de1a9369f287aef388622966175171522f5
jlatour88 <jlatour88@gmail.com>
2019-11-11 22:07:51 -0600

    added default profile image with conditional check

--- client/src/pages/Students/Students.js
+++ client/src/pages/Students/Students.js
@@ -1,19 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import Header from '../../components/Header/Header'
+import Header from '../../components/Header/Header';
+import ProfilePlaceholder from '../../assets/images/profile-placeholder.png';
 
 const Students = () => {
   const [data, setData] = useState({ studentArr: [] });
 
   useEffect(() => {
-    axios('/api/findStudents')
-    .then(students => setData({studentArr: students.data}))
+    axios('/api/findStudents').then(students =>
+      setData({ studentArr: students.data })
+    );
   }, []);
   console.log(data.studentArr);
 
   return (
     <div>
-      <Header miniHeader title='Student List'/>
+      <Header miniHeader title='Student List' />
       <table className='table'>
         <thead className='thead-dark'>
           <tr>
@@ -30,10 +32,14 @@ const Students = () => {
           {data.studentArr.map(student => (
             <tr key={student.id}>
               <td>
-                <img 
-                  src={student.profileImage} 
-                  alt='' 
-                  style={{height: '30px'}}
+                <img
+                  src={
+                    student.profileImage
+                      ? student.profileImage
+                      : ProfilePlaceholder
+                  }
+                  alt=''
+                  style={{ height: '30px' }}
                 />
               </td>
               <td>{student.firstName}</td>
@@ -48,6 +54,6 @@ const Students = () => {
       </table>
     </div>
   );
-}
+};
 
-export default Students
+export default Students;
