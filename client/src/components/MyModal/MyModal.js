bdb813a4704f23bbcf3c95054de6ee654bc5bfe2
Josh Latour <jlatour88@gmail.com>
2019-11-14 10:27:34 -0600

    added some conditional checks

--- client/src/components/MyModal/MyModal.js
+++ client/src/components/MyModal/MyModal.js
@@ -3,27 +3,25 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
 const MyModal = props => {
-  // const [show, setShow] = useState(false);
-
-  // const handleClose = () => setShow(false);
-  // const handleShow = () => setShow(true);
-
   return (
     <>
-      {/* <Button variant='primary' onClick={handleShow}>
-        Launch demo modal
-      </Button> */}
-
       <Modal show={props.showModal} onHide={props.handleModalClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>{props.title}</Modal.Title>
-        </Modal.Header>
+        {props.title &&
+          <Modal.Header closeButton>
+            <Modal.Title>{props.title}</Modal.Title>
+          </Modal.Header>
+        }
         <Modal.Body>{props.children}</Modal.Body>
-        <Modal.Footer>
-          <Button variant='secondary' onClick={props.handleModalClose}>
-            Close
-          </Button>
-        </Modal.Footer>
+        {props.confirmation &&
+          <Modal.Footer>
+            <Button variant='primary' onClick={props.handleModalClose}>
+              Continue
+            </Button>
+            <Button variant='secondary' onClick={props.handleModalClose}>
+              Close
+            </Button>
+          </Modal.Footer>
+        }
       </Modal>
     </>
   );
