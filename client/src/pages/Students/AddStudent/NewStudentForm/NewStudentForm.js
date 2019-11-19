d9c07d116bbbe8bb2ce8573e876eed44f2f8fe75
jlatour88 <jlatour88@gmail.com>
2019-11-18 21:02:55 -0600

    added basic styling, phone parse, labels

--- client/src/pages/Students/AddStudent/NewStudentForm/NewStudentForm.js
+++ client/src/pages/Students/AddStudent/NewStudentForm/NewStudentForm.js
@@ -5,6 +5,17 @@ import { FieldArray } from 'react-final-form-arrays';
 
 import styles from './NewStudentForm.module.css';
 
+const normalizePhone = value => {
+  if (!value) return value;
+  const onlyNums = value.replace(/[^\d]/g, "");
+  if (onlyNums.length <= 3) return onlyNums;
+  if (onlyNums.length <= 7)
+    return `(${onlyNums.slice(0, 3)}) ${onlyNums.slice(3, 7)}`;
+  return `(${onlyNums.slice(0, 3)}) ${onlyNums.slice(3, 6)}-${onlyNums.slice(
+    6,
+    10
+  )}`;
+};
 
 const NewStudentForm = (props) => (
   <Form
@@ -27,29 +38,36 @@ const NewStudentForm = (props) => (
         <form onSubmit={handleSubmit}>
           <h3>Parents Info</h3>
           <div>
+            <label>First Name</label>
             <Field
               name='parentObj.firstName'
-              placeholder='First Name'
+              placeholder='John'
               component='input' />
+            <label>First Name2 <sup>optional</sup></label>
             <Field
               name='parentObj.firstName2'
-              placeholder='First Name(optional)'
+              placeholder='Mary'
               component='input' />
+            <label>Last Name</label>
             <Field
               name='parentObj.lastName'
-              placeholder='Last Name'
+              placeholder='Smith'
               component='input' />
+            <label>Email</label>
             <Field
               name='parentObj.email'
-              placeholder='Email'
+              placeholder='test@test.com'
               component='input' />
+            <label>Phone</label>
             <Field
               name='parentObj.phone'
-              placeholder='Phone'
+              placeholder='(999) 999-9999'
+              parse={normalizePhone}
               component='input' />
+            <label>Address</label>
             <Field
               name='parentObj.address'
-              placeholder='Address'
+              placeholder='123 Test Lane'
               component='input' />
           </div>
 
@@ -85,36 +103,43 @@ const NewStudentForm = (props) => (
                     </span>
                   </h5>
                   <div>
+                    <label>First Name</label>
                     <Field
                       name={`${name}.firstName`}
                       component='input'
-                      placeholder='First Name' />
+                      placeholder='Bobby' />
+                    <label>Last Name</label>
                     <Field
                       name={`${name}.lastName`}
                       component='input'
                       initialValue={values.parentObj.lastName}
-                      placeholder='Last Name' />
+                      placeholder='Smith' />
+                    <label>Birthdate</label>
                     <Field
                       name={`${name}.birthdate`}
                       component='input'
                       type='date'
-                      placeholder='BirthDate' />
+                      placeholder='01/01/2012' />
+                    <label>Gender</label><br />
                     <Field
                       name={`${name}.gender`}
                       component='select'
                     >
-                      <option value=''>Gender</option>
+                      <option value='default'>Choose One</option>
                       <option value='male'>Male</option>
                       <option value='female'>Female</option>
                     </Field>
+                    <br />
+                    <label>Allergies</label>
                     <Field
                       name={`${name}.allergies`}
                       component='input'
-                      placeholder='Allergies' />
+                      placeholder='Peanuts' />
+                    <label>Medical</label><br />
                     <Field
                       name={`${name}.medical`}
                       component='textarea'
-                      placeholder='Medical' />
+                      placeholder='' />
                   </div>
                 </div>
               ))
