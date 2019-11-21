fabb36e4a68d102832c84736abeb5fb78836f890
Josh Latour <jlatour88@gmail.com>
2019-11-21 14:04:55 -0600

    Started adding content to modal.

--- client/src/pages/Students/StudentCard/StudentCard.js
+++ client/src/pages/Students/StudentCard/StudentCard.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import Card from 'react-bootstrap/Card';
 import MyModal from '../../../components/MyModal/MyModal';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import ProfilePlaceholder from '../../../assets/images/profile-placeholder.png';
 
 
@@ -55,9 +58,36 @@ const StudentCard = (props) => {
         showModal={show} 
         handleModalClose={handleClose}
       >
-        <p>Allergies - {modalData.student.allergies}</p>
-        <p>Parent Phone - {modalData.parent.phone}</p>
-        <p>Medical - {modalData.student.medical}</p>
+        <Container>
+          {modalData.student.profileImage &&
+          <Row>
+            <Col>Age</Col><Col>{modalData.student.profileImage}</Col>
+          </Row> }
+          <Row>
+            <Col>Age</Col><Col>{modalData.student.birthdate}</Col>
+          </Row>
+          <Row>
+            <Col>Allergies</Col><Col>{modalData.student.allergies}</Col>
+          </Row>
+          <Row>
+            <Col>Medical</Col><Col>{modalData.student.medical}</Col>
+          </Row>
+
+          <Row>
+            <Col style={{fontWeight: 'bold', textAlign: 'center'}}>Parents Contact Info</Col>
+          </Row>
+          <Row>
+            <Col>Name</Col>
+            <Col>{modalData.parent.firstName} & {modalData.parent.firstName2}</Col>
+          </Row>
+          <Row>
+            <Col>Phone</Col><Col>{modalData.parent.phone}</Col>
+          </Row>
+          <Row>
+            <Col>Emergency Contact</Col>
+          <Col>{modalData.parent.emergencyName} | {modalData.parent.emergencyPhone}</Col>
+          </Row>
+        </Container>
       </MyModal>
     </main>
   )
