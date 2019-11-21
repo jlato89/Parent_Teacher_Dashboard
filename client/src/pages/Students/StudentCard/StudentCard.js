0dd222d6c8e6c93fc69c42d035ff4cf71f078e0b
Josh Latour <jlatour88@gmail.com>
2019-11-20 23:47:20 -0600

    added modal to display more info on student and parent

--- client/src/pages/Students/StudentCard/StudentCard.js
+++ client/src/pages/Students/StudentCard/StudentCard.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from 'react-bootstrap/Card';
+import MyModal from '../../../components/MyModal/MyModal';
 import ProfilePlaceholder from '../../../assets/images/profile-placeholder.png';
+
 
 // import styles from './StudentCard.module.css';
 
 const StudentCard = (props) => {
+  const [show, setShow] = useState(false);
+  const [modalData, setModalData] = useState({ student: '', parent: '' });
+
+  const handleClose = () => setShow(false);
+  const handleShow = (student) => {
+    setShow(true);
+    setModalData({
+      student: student,
+      parent: parentInfoHandler(student.parentId)
+    })
+  }
+
   const parentInfoHandler = (parentId) => {
-    let obj=  props.parentArr.find(p => p.id === parentId)
+    let obj = props.parentArr.find(p => p.id === parentId)
     return obj
   }
   return (
     <main>
       {props.studentArr.map(student => (
-        <Card className='d-flex flex-row' key={student.id}>
+        <Card
+          className='d-flex flex-row'
+          key={student.id}
+          onClick={() => handleShow(student)}
+        >
           <Card.Img variant="top" style={{ width: '35%' }} src={
             student.profileImage
               ? student.profileImage
@@ -32,6 +50,15 @@ const StudentCard = (props) => {
           </Card.Body>
         </Card>
       ))}
+      <MyModal 
+        title={modalData.student.firstName + ' ' + modalData.student.lastName} 
+        showModal={show} 
+        handleModalClose={handleClose}
+      >
+        <p>Allergies - {modalData.student.allergies}</p>
+        <p>Parent Phone - {modalData.parent.phone}</p>
+        <p>Medical - {modalData.student.medical}</p>
+      </MyModal>
     </main>
   )
 }
