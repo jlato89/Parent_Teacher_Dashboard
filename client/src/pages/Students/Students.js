098a1447d68a5936ea896186dc27e3c85fb77447
Josh Latour <jlatour88@gmail.com>
2019-11-22 12:58:12 -0600

    clean up unused code

--- client/src/pages/Students/Students.js
+++ client/src/pages/Students/Students.js
@@ -6,9 +6,6 @@ const Students = () => {
   const [data, setData] = useState({ studentArr: [], parentArr: [] });
 
   useEffect(() => {
-    // axios('/api/findStudents').then(students =>
-    //   setData({ studentArr: students.data })
-    // );
     axios
       .all([
         axios.get('/api/findStudents'),
@@ -32,42 +29,6 @@ const Students = () => {
     <div>
       <Header miniHeader title='Student List' />
       <StudentCard studentArr={data.studentArr} parentArr={data.parentArr} />
-      {/* <table className='table'>
-        <thead className='thead-dark'>
-          <tr>
-            <th></th>
-            <th>First</th>
-            <th>Last</th>
-            <th>Gender</th>
-            <th>Parent</th>
-            <th>Allergies</th>
-            <th>Medical</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.studentArr.map(student => (
-            <tr key={student.id}>
-              <td>
-                <img
-                  src={
-                    student.profileImage
-                      ? student.profileImage
-                      : ProfilePlaceholder
-                  }
-                  alt=''
-                  style={{ height: '30px' }}
-                />
-              </td>
-              <td>{student.firstName}</td>
-              <td>{student.lastName}</td>
-              <td>{student.gender}</td>
-              <td>{student.parentId}</td>
-              <td>{student.allergies}</td>
-              <td>{student.medical}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table> */}
     </div>
   );
 };
