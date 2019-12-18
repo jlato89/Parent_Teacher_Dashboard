cfcceef6558debc4d1f94893af9a558bccf5dc34
Josh Latour <jlatour88@gmail.com>
2019-12-18 09:52:02 -0600

    Moved DashBtns to component and setup check for user access type

--- client/src/components/DashBtns/DashBtns.js
+++ client/src/components/DashBtns/DashBtns.js
@@ -2,10 +2,56 @@ import React from 'react';
 import DashButton from './DashButton/DashButton';
 import styles from './DashBtns.module.css';
 
+const menuBtns = [
+  {
+    name: 'Create Report',
+    url: '/createReport',
+    access: 'teacher'
+  },
+  {
+    name: 'Add Students',
+    url: '/addStudent',
+    access: 'teacher'
+  },
+  {
+    name: 'Events',
+    url: '/events',
+    access: 'teacher'
+  },
+  {
+    name: 'Students',
+    url: '/students',
+    access: 'teacher'
+  },
+  {
+    name: 'Reports',
+    url: '/reports',
+    access: 'teacher'
+  },
+  {
+    name: 'Your Profile',
+    url: '/profile',
+    access: 'teacher'
+  }
+]
+
 const DashBtns = props => {
+  let btnsToDisplay;
+  //? Menu Buttons depending on user access
+  switch (props.access) {
+    case 'admin':
+      btnsToDisplay = menuBtns.filter(el => el.access === 'admin');
+      break;
+    case 'teacher':
+      btnsToDisplay = menuBtns.filter(el => el.access === 'teacher');
+      break;
+    default:
+      btnsToDisplay = menuBtns.filter(el => el.access === 'parent');
+      break;
+  }
   return (
     <div className={styles.container}>
-      <DashButton buttons={props.dashBtns} />
+      <DashButton buttons={btnsToDisplay} />
     </div>
   )
 }
--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -2,10 +2,9 @@ import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 import axios from 'axios';
 import setAuthToken from '../../utils/setAuthToken';
-// import jwt_decode from 'jwt-decode';
 import Layout from '../../components/Layout/Layout';
-import Announcements from '../../components/Events/Announcement/Announcement';
 import DashBtns from '../../components/DashBtns/DashBtns';
+import Announcements from '../../components/Events/Announcement/Announcement';
 import UpcomingEvents from '../../components/Events/UpcomingEvents/UpcomingEvents';
 import Loading from '../../components/UI/Loading/Loading';
 
@@ -18,50 +17,15 @@ class Dashboard extends Component {
       user: {},
       eventArr: [],
       reportArr: [],
-      dashBtns: [
-        {
-          name: 'Create Report',
-          url: '/createReport'
-        },
-        {
-          name: 'Add Students',
-          url: '/addStudent'
-        },
-        {
-          name: 'Events',
-          url: '/events'
-        },
-        {
-          name: 'Students',
-          url: '/students'
-        },
-        {
-          name: 'Reports',
-          url: '/reports'
-        },
-        {
-          name: 'Your Profile',
-          url: '/profile'
-        }
-      ]
     };
   }
 
   async componentDidMount() {
     //? Check for token in localStorage, if true set default headers
     const token = localStorage.getItem('ptDash');
-    if (token) {
-      setAuthToken(token);
-      // const decode = jwt_decode(token);
-      // await this.setState({
-      //   user: {
-      //     id: decode.id,
-      //     userType: decode.userType
-      //   }
-      // });
-    }
+    if (token) setAuthToken(token);
 
-    //? Run all calls for info that needs to be retrieved.
+    //? Start initial Axios Calls
     axios
       .all([
         axios.get('/api/findUser'),
@@ -87,20 +51,18 @@ class Dashboard extends Component {
   };
 
   render() {
-    const { redirect, user } = this.state;
+    const { redirect, user, eventArr } = this.state;
+    let dashboardContent;
 
     //? Redirect to homepage if logged-out
     if (redirect) {
       return <Redirect to='/' />;
     }
-
-    //? --------------------------------------------------------
     //? Content to render when app is loading
-    let dashboardContent;
     if (this.state.loading) {
       dashboardContent = <Loading />;
     }
-    //? Main content to render once app is finished loading
+    //? Content to render when app is finished loading
     else {
       dashboardContent = (
         <>
@@ -109,15 +71,14 @@ class Dashboard extends Component {
             profileImg={user.profileImage}
             name={user.firstName}
           >
-            <Announcements events={this.state.eventArr} />
-            <DashBtns dashBtns={this.state.dashBtns} />
-            <UpcomingEvents events={this.state.eventArr} />
+            <Announcements events={eventArr} />
+            <DashBtns access={user.userType} />
+            <UpcomingEvents events={eventArr} />
           </Layout>
         </>
       );
     }
-
-    return <div className='dashboardPage'>{dashboardContent}</div>;
+    return <>{dashboardContent}</>;
   }
 }
 
