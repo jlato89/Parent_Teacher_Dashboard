39a35ccee14e220214da84ba1b8ea5b9bc38c4b7
Josh Latour <jlatour88@gmail.com>
2020-01-21 11:59:05 -0600

    Possible fix for props not being passed back

--- client/src/utils/PrivateRoute.js
+++ client/src/utils/PrivateRoute.js
@@ -2,21 +2,21 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import Authenticate from './Authenticate';
 
-const PrivateRoute = ({ component: Component, ...rest }) => {
+const PrivateRoute = ({ component: Component, ...props }) => {
   return (
     <Route
-      {...rest}
-      render={props =>
+      {...props}
+      render={rest =>
         Authenticate() === true ? (
           <Component {...props} />
         ) : (
-          <Redirect
-            to={{
-              pathname: '/',
-              state: { from: props.location }
-            }}
-          />
-        )
+            <Redirect
+              to={{
+                pathname: '/',
+                state: { from: props.location }
+              }}
+            />
+          )
       }
     />
   );
