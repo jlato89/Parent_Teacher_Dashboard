d8fcc7703b69f8aee77f51439c15decbb19fae02
Josh Latour <jlatour88@gmail.com>
2020-01-24 00:33:54 -0600

    Changed to Stateless Compoenent, Added UserProvider Context API with logged in user

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,33 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import PrivateRoute from './utils/PrivateRoute';
+import { UserProvider } from './UserContext';
+import setAuthToken from './utils/setAuthToken';
+import axios from 'axios';
+
 import Login from './pages/Login/Login';
 import Dashboard from './pages/Dashboard/Dashboard';
-import NoMatch from './pages/NoMatch';
-
+import Profile from './pages/Profile/Profile';
 import CreateReport from './pages/Reports/CreateReport/CreateReport';
 import Students from './pages/Students/Students';
 import EventList from './pages/EventList/EventList';
 import Reports from './pages/Reports/Reports';
 import AddStudent from './pages/Students/AddStudent/AddStudent';
-import Profile from './pages/Profile/Profile';
+import NoMatch from './pages/NoMatch';
 
-class App extends Component {
-  render() {
-    return (
+const App = () => {
+  const [user, setUser] = useState();
+  const token = localStorage.getItem('ptDash');
+
+  if (!user && token) {
+    console.log('[APP] Setting current user to state');
+    setAuthToken(token);
+    setUser({ userId: 123, name: 'Josh L' }) // Inittial testing data
+    axios('/api/findUser').then(user => setUser(user.data.user)); //! Not being set on first render
+  }
+
+  return (
+    <UserProvider value={user}>
       <Router>
         <Switch>
           <Route exact path='/' component={Login} />
@@ -29,8 +42,8 @@ class App extends Component {
           <Route component={NoMatch} />
         </Switch>
       </Router>
-    );
-  }
+    </UserProvider>
+  );
 }
 
 export default App;
