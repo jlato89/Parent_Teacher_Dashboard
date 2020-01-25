e984b8aeeac5f78a76134ff82fcdcb9389449ab1
jlatour88 <jlatour88@gmail.com>
2020-01-25 01:06:20 -0600

    Refactored and cleaned up code

--- client/src/utils/PrivateRoute.js
+++ client/src/utils/PrivateRoute.js
@@ -2,24 +2,14 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import Authenticate from './Authenticate';
 
-const PrivateRoute = ({ component: Component, ...props }) => {
-  return (
-    <Route
-      {...props}
-      render={rest =>
-        Authenticate() === true ? (
-          <Component {...props} />
-        ) : (
-            <Redirect
-              to={{
-                pathname: '/',
-                state: { from: props.location }
-              }}
-            />
-          )
-      }
-    />
-  );
-};
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route {...rest}
+    render={(props) =>
+      Authenticate() === true
+        ? <Component {...props} />
+        : <Redirect to={{ pathname: '/', state: { from: props.location } }} />
+    }
+  />
+);
 
-export default PrivateRoute;
+export default PrivateRoute
