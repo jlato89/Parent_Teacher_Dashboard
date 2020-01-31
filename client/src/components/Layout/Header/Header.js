3b40e09ac11aae77c2e43869c0dc4073a820302a
Josh Latour <jlatour88@gmail.com>
2020-01-30 22:40:12 -0600

    Updated for CSS Grid, code cleaned up

--- client/src/components/Layout/Header/Header.js
+++ client/src/components/Layout/Header/Header.js
@@ -3,50 +3,38 @@ import UserContext from '../../../UserContext';
 import { MailLink, SearchLink, HomeLink } from './Components/IconLinks';
 import Title from './Components/Title';
 import ProfileImg from './Components/ProfileImg';
-import WelcomeText from './Components/WelcomeText';
+import Greeting from './Components/Greeting';
 import LogoutBtn from './Components/LogoutBtn';
 import styles from './Header.module.css';
 
-const Header = (props) => {
+const Header = ({ title, miniHeader }) => {
   const { user, logoutUser } = useContext(UserContext);
-  let headerContent;
-
-  if (props.miniHeader) {
-    headerContent = (
-      <div className={styles.topHeader}>
-        {/* Home Button */}
-        <HomeLink style={styles.homeIcon} />
-        {/* Title */}
-        <Title style={styles.topHeaderText} title={props.title} />
-        {/* Search Button */}
-        <SearchLink style={styles.searchIcon} />
-      </div>
-    )
-  } else {
-    headerContent = (
-      <>
-        <div className={styles.topHeader}>
-          {/* Left Button */}
-          <MailLink style={styles.mailIcon} />
-          {/* Profile Img */}
-          <ProfileImg style={styles.profileImg} image={user.profileImage} />
-          {/* Right Button */}
-          <SearchLink style={styles.searchIcon} />
-        </div>
-
-        <div className={styles.bottomHeader}>
-          {/* Welcome Message */}
-          <WelcomeText style={styles.bottomHeader} name={user.firstName} />
-          {/* Logout Button */}
-          <LogoutBtn style={styles.logoutIcon} logoutHandler={logoutUser} />
-        </div>
-      </>
-    )
-  }
 
   return (
-    <div className={styles.wrapper}>
-      {headerContent}
+    <div className={styles.container}>
+      {miniHeader ?
+        <>
+          {/* Home Button */}
+          <HomeLink style={styles.leftBtn} />
+          {/* Title */}
+          <Title style={styles.title} title={title} />
+          {/* Search Button */}
+          <SearchLink style={styles.rightBtn} />
+        </>
+        :
+        <>
+          {/* Left Button */}
+          <MailLink style={styles.leftBtn} />
+          {/* Profile Img */}
+          <ProfileImg style={styles.profileImg} image={user.profileImage} />
+          {/* Right Button  */}
+          <SearchLink style={styles.rightBtn} />
+          {/* Greeting Message  */}
+          <Greeting style={styles.greeting} name={user.firstName} />
+          {/* Logout Button  */}
+          <LogoutBtn style={styles.logoutIcon} logoutHandler={logoutUser} />
+        </>
+      }
     </div>
   );
 };
