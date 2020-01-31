88d12c6c683600c3f1a9d580ee7d6b013e4a525e
Josh Latour <jlatour88@gmail.com>
2020-01-30 21:31:08 -0600

    Moved content to specific Components

--- client/src/components/Layout/Header/Header.js
+++ client/src/components/Layout/Header/Header.js
@@ -1,21 +1,11 @@
 import React, { useContext } from 'react';
 import UserContext from '../../../UserContext';
-import { Link } from 'react-router-dom';
-import Moment from 'react-moment';
-import MailIcon from '../../../assets/images/outline_mail_white_24.png';
-import SearchIcon from '../../../assets/images/outline_search_white_24.png';
-import HomeIcon from '../../../assets/images/outline_home_white_18dp.png';
-import MenuBtn from '../../../assets/images/outline_menu_white_24.png';
-import LogoutIcon from '../../../assets/images/outline_directions_run_white_18dp.png';
-import ProfileImgDefault from '../../../assets/images/outline_account_circle_white_48.png';
+import { MailLink, SearchLink, HomeLink } from './Components/IconLinks';
+import Title from './Components/Title';
+import ProfileImg from './Components/ProfileImg';
+import WelcomeText from './Components/WelcomeText';
+import LogoutBtn from './Components/LogoutBtn';
 import styles from './Header.module.css';
-
-const date = new Date();
-const curHr = date.getHours();
-let greetingMsg;
-if (curHr < 12) { greetingMsg = 'Good Morning'; }
-else if (curHr < 18) { greetingMsg = 'Good Afternoon'; }
-else { greetingMsg = 'Good Evening'; }
 
 const Header = (props) => {
   const { user, logoutUser } = useContext(UserContext);
@@ -24,16 +14,12 @@ const Header = (props) => {
   if (props.miniHeader) {
     headerContent = (
       <div className={styles.topHeader}>
-        {/* Left Button */}
-        <Link to='/dashboard'>
-          <img src={HomeIcon} alt='Home' height='30px' />
-        </Link>
-        {/* Middle */}
-        <span className={styles.topHeaderText}>
-          {props.title ? props.title : 'PT Dashboard'}
-        </span>
-        {/* Right Button */}
-        <img src={MenuBtn} alt='Menu' height='30px' />
+        {/* Home Button */}
+        <HomeLink style={styles.homeIcon} />
+        {/* Title */}
+        <Title style={styles.topHeaderText} title={props.title} />
+        {/* Search Button */}
+        <SearchLink style={styles.searchIcon} />
       </div>
     )
   } else {
@@ -41,29 +27,18 @@ const Header = (props) => {
       <>
         <div className={styles.topHeader}>
           {/* Left Button */}
-          <Link to='/mail'>
-            <img src={MailIcon} alt='Mail' height='30px' />
-          </Link>
+          <MailLink style={styles.mailIcon} />
           {/* Profile Img */}
-          <img
-            className={styles.profileImg}
-            src={user.profileImage ? user.profileImage : ProfileImgDefault}
-            alt='Profile Img'
-          />
+          <ProfileImg style={styles.profileImg} image={user.profileImage} />
           {/* Right Button */}
-          <Link to='/search'>
-            <img src={SearchIcon} alt='Search' height='30px' />
-          </Link>
+          <SearchLink style={styles.searchIcon} />
         </div>
+
         <div className={styles.bottomHeader}>
-          <p><strong>Hi {user.firstName}, </strong>{greetingMsg}</p>
-          <span><Moment format='ddd, MMM Do YYYY' date={date} /></span>
-          <img
-            className={styles.logoutIcon}
-            src={LogoutIcon}
-            alt='Logout'
-            onClick={logoutUser}
-          />
+          {/* Welcome Message */}
+          <WelcomeText style={styles.bottomHeader} name={user.firstName} />
+          {/* Logout Button */}
+          <LogoutBtn style={styles.logoutIcon} logoutHandler={logoutUser} />
         </div>
       </>
     )
